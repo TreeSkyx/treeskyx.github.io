b61cbd47599ffec2c0087eecc9b713f9a6323ac2
TreeSkyx <88649190+TreeSkyx@users.noreply.github.com>
2023-12-27 00:36:18 +0700

    update home page

--- my-portfolio/src/Portfolio.js
+++ my-portfolio/src/Portfolio.js
@@ -1,6 +1,7 @@
 // Portfolio.js
 import React from 'react';
 import './Portfolio.css'; // Create this file for styling
+import profilePicture from './assets/TF_img.JPG'; // Replace with the actual path to your picture
 
 const Portfolio = () => {
   return (
@@ -11,8 +12,17 @@ const Portfolio = () => {
           <div className="minimize-button"></div>
           <div className="maximize-button"></div>
         </div>
-        {/* Add your content here */}
-        <h1>Your Portfolio Content Goes Here</h1>
+        <div className="content-container">
+          <div className="portfolio-content">
+            <h1>Hello, It's Me</h1>
+            <h1>Wiroon Somphaothong</h1>
+            <h1>I'm FrontEnd developer</h1>
+            <button className="about-me-button">About Me</button>
+          </div>
+          <div className="profile-picture-container">
+            <img src={profilePicture} alt="Profile" className="profile-picture" />
+          </div>
+        </div>
       </div>
     </div>
   );
