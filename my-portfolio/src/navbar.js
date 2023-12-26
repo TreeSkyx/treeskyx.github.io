cb144563cde9056f10fd2f4539043e89095147b7
TreeSkyx <88649190+TreeSkyx@users.noreply.github.com>
2023-12-26 23:31:55 +0700

    create project and navbar

---
+++ my-portfolio/src/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="logo">Your Name</div>
+        <ul className="nav-links">
+          <li><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
