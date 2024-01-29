f0a5fe246afdea5c101fd7a5efaa9b803ffb10e7
TreeSkyx <88649190+TreeSkyx@users.noreply.github.com>
2024-01-30 04:48:28 +0700

    update the contact page

--- my-portfolio/src/Contact.js
+++ my-portfolio/src/Contact.js
@@ -2,21 +2,46 @@ import React from "react";
 import "./Contact.css";
 
 const Contact = () => {
+  const githubProfileUrl = "https://github.com/TreeSkyx";
+  const linkedinProfileUrl = "https://www.linkedin.com/in/wiroon-somphaothong/"; // Replace with your LinkedIn profile URL
+  const gmailAddress = "mailto:wiroom.som@gmail.com";
+
   return (
-    <div className="projects">
-      <div className="projects-window">
+    <div className="contact">
+      <div className="contact-window">
         <div className="title-bar">
           <div className="close-button"></div>
           <div className="minimize-button"></div>
           <div className="maximize-button"></div>
         </div>
         <div className="content-container">
-          <div className="projects-content">
-            <div className="title">Projects</div>
+          <div className="contact-content">
+            <div className="title">Stay Connected: How to Reach Me</div>
             <div className="description">
-              Here are some of my projects from the past:
+              Let's stay connected and explore job opportunities together! As a
+              university student ready to dive into the professional world, I'm
+              excited to connect with you. Drop me a message using the channels
+              below and let's chat about potential gigs and collaborations.
+              Can't wait to hear from you!
             </div>
-            <div className="project-list">
+            <div className="homepage-social">
+              <a
+                href={githubProfileUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-github"></i>
+              </a>
+              <a
+                href={linkedinProfileUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-linkedin"></i>
+              </a>
+              <a href={gmailAddress} target="_blank" rel="noopener noreferrer">
+                <i className="far fa-envelope"></i>{" "}
+              </a>
             </div>
           </div>
         </div>
